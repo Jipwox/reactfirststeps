ef09c93ab75764e74a7c3d064f8f35418eb0263a
Jipwox <johnphilip.cardi@gmail.com>
2022-05-30 09:08:46 -0700

    learning about components and props

--- react-first-steps/src/App.js
+++ react-first-steps/src/App.js
@@ -1,10 +1,41 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Boat Insurance",
+      amount: 342.88,
+      date: new Date(2021, 1, 19),
+    },
+    {
+      id: "e3",
+      title: "Home Insurance",
+      amount: 472.67,
+      date: new Date(2021, 5, 7),
+    },
+    {
+      id: "e4",
+      title: "Motorcycle Insurance",
+      amount: 98.22,
+      date: new Date(2021, 3, 10),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem expenses={expenses[0]}/>
+      <ExpenseItem expenses={expenses[1]}/>
+      <ExpenseItem expenses={expenses[2]}/>
+      <ExpenseItem expenses={expenses[3]}/>
+      
     </div>
   );
 }
--- react-first-steps/src/components/ExpenseItem.js
+++ react-first-steps/src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 28).toDateString();
-  const expenseTitle = 'Boat Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  const month = props.expenses.date.toLocaleString('en-US', {month: 'long'})
+  const day = props.expenses.date.toLocaleString('en-US', {day: '2-digit'})
+  const year = props.expenses.date.getFullYear()
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate}</div>
+      <ExpenseDate date={props.expenses.date}/>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{props.expenses.title}</h2>
+        <div className='expense-item__price'>{props.expenses.amount}</div>
       </div>
     </div>
   );
