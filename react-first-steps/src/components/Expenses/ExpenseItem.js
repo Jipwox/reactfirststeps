e25bfd605c2760264a127cc7ee7fe2bc507ef4b9
Jipwox <johnphilip.cardi@gmail.com>
2022-05-31 15:08:31 -0700

    beginning state

--- react-first-steps/src/components/Expenses/ExpenseItem.js
+++ react-first-steps/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
+import {useState} from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-  
+
+  const [title, setTitle] = useState(props.expenseItem.title);
+
   const clickHandler = () => {
-    console.log('Clicked!');
+    setTitle("NootNoot!");
   }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.expenseItem.date}/>
       <div className='expense-item__description'>
-        <h2>{props.expenseItem.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{props.expenseItem.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
