f67fc8c00c843b4a412ada85d0426ab7f0c6508a
Jipwox <johnphilip.cardi@gmail.com>
2022-06-06 15:02:26 -0700

    Added expanded and collapsing new expense menu

--- react-first-steps/src/components/NewExpense/NewExpense.js
+++ react-first-steps/src/components/NewExpense/NewExpense.js
@@ -1,18 +1,39 @@
+import { useState } from 'react';
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+
+    const [formCollapsed, setFormCollapsed] = useState(true);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
         };
-        props.onSaveExpense(expenseData)
+        props.onSaveExpense(expenseData);
+        collapseNewExpenseHandler();
     };
+
+    const expandNewExpenseHandler = () => {
+        setFormCollapsed(false);
+    }
+
+    const collapseNewExpenseHandler = () => {
+        setFormCollapsed(true);
+    }
+
+    const addNewExpenseBtn = <button onClick={expandNewExpenseHandler}>Add New Expense</button>;
+
+    const expenseForm = <ExpenseForm 
+                            onSaveExpenseData={saveExpenseDataHandler} 
+                            onCancelAddExpense={collapseNewExpenseHandler}    
+                        />
 
     return(
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {formCollapsed && addNewExpenseBtn}
+            {!formCollapsed && expenseForm}
         </div>
     );
 };
